0bebe8ad6e7df413580b21224b5f522a5600bfd0
Dronza79 <113519920+Dronza79@users.noreply.github.com>
2024-07-24 15:42:02 +0300

    Add files via upload

--- database/queries.py
+++ database/queries.py
@@ -1,3 +1,8 @@
 def get_all_workers():
     from .models import Person
     return list(Person.select())
+
+
+def get_all_tasks():
+    from .models import WorkTask
+    return list(WorkTask.select())
