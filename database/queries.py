d848eb425c25c6ee6fdf333fb9260be386249e1c
Dronza79 <113519920+Dronza79@users.noreply.github.com>
2024-07-26 15:54:00 +0300

    Add files via upload

--- database/queries.py
+++ database/queries.py
@@ -1,8 +1,12 @@
+import peewee
+from .models import Person, FuncPosition, WorkTask
+
+
 def get_all_workers():
-    from .models import Person
-    return list(Person.select())
+    person = Person.select(Person, FuncPosition).join(FuncPosition)
+    tasks = WorkTask.select()
+    return peewee.prefetch(person, tasks)
 
 
 def get_all_tasks():
-    from .models import WorkTask
-    return list(WorkTask.select())
+    return WorkTask.select(WorkTask, Person).join(Person)
