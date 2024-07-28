f52c85826b9f7a762979e3f1f738671e2f62ebd2
Зайцев Андрей <mr_zaycev@mail.ru>
2024-07-28 22:23:27 +0300

    новые запросы к БД

--- database/queries.py
+++ database/queries.py
@@ -1,25 +1,52 @@
 import peewee
-from .models import Person, FuncPosition, WorkTask, Status
+from .models import Person, FuncPosition, WorkTask, Status, WorkLapse
 
 
 def get_all_workers():
     person = Person.select(Person, FuncPosition.title).join(FuncPosition)
     tasks = WorkTask.select(WorkTask, Status).join(Status).where(Status.state == 'В работе')
-    return peewee.prefetch(person, tasks)
+    # return peewee.prefetch(person, tasks)
+    return (
+        Person.select(
+            Person.id, Person.surname, Person.name, Person.second_name,
+            FuncPosition.title,
+            WorkTask.order, WorkTask.deadline, peewee.fn.SUM(WorkLapse.term).alias('total'),
+            Status.state,
+        )
+        .join_from(Person, FuncPosition)
+        .join_from(Person, WorkTask, peewee.JOIN.LEFT_OUTER)
+        .join_from(WorkTask, Status)
+        .join_from(WorkTask, WorkLapse, peewee.JOIN.LEFT_OUTER)
+        .where(Status.state == 'В работе')
+        .group_by(Person.id).dicts()
+    )
 
 
 def get_all_tasks():
     return (
         WorkTask.select(
-            WorkTask.id, WorkTask.equipment, WorkTask.title, WorkTask.article, WorkTask.order, WorkTask.deadline,
-            WorkTask.duration,
+            WorkTask.id, WorkTask.equipment, WorkTask.title, WorkTask.article, WorkTask.order,
+            WorkTask.deadline, peewee.fn.SUM(WorkLapse.term).alias('total'),
             Status.state,
             Person.surname, Person.name, Person.second_name
         )
-        .join(Status)
+        .join_from(WorkTask, WorkLapse, peewee.JOIN.LEFT_OUTER)
+        .join_from(WorkTask, Status)
+        .join_from(WorkTask, Person)
         .where(Status.state == 'В работе')
-        .switch(WorkTask)
-        .join(Person)
+        .group_by(WorkTask.id).dicts()
+
+        # WorkTask.select(
+        #     WorkTask.id, WorkTask.equipment, WorkTask.title, WorkTask.article, WorkTask.order, WorkTask.deadline,
+        #     WorkTask.duration,
+        #     Status.state,
+        #     Person.surname, Person.name, Person.second_name
+        # )
+        # .join(Status)
+        # .where(Status.state == 'В работе')
+        # .switch(WorkTask)
+        # .join(Person)
     )
-            # .join(Person).dicts())
+
+
 
