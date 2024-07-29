471bc1c8ccfceca3770b482348f39c810ce87562
Dronza79 <113519920+Dronza79@users.noreply.github.com>
2024-07-29 16:28:11 +0300

    Add files via upload

--- database/models.py
+++ database/models.py
@@ -44,57 +44,50 @@ class Status(BaseModel):
 
 
 class WorkTask(BaseModel):
-    equipment = CharField(verbose_name='Тип объекта')
+    type_obj = CharField(verbose_name='Тип объекта')
     title = CharField(verbose_name='Наименование объекта')
     article = CharField(verbose_name='Конструктив')
     order = CharField(verbose_name='Производственный заказ')
     deadline = SmallIntegerField(verbose_name='Норматив выполнения')
     master = ForeignKeyField(Person, backref='tasks', verbose_name='Работник', on_delete='CASCADE')
     status = ForeignKeyField(Status, backref='tasks', verbose_name='Состояние', default=1, on_delete='CASCADE')
-    duration = SmallIntegerField(verbose_name="Общая продолжительность", default=0)
     comment = TextField(verbose_name='Комментарий', null=True)
 
     def __str__(self):
-        # return f'{self.order} ({self.status})'
         return self.order
 
 
 class WorkLapse(BaseModel):
     worker = ForeignKeyField(Person, verbose_name='Работник', backref='time_worked', on_delete='CASCADE')
     task = ForeignKeyField(WorkTask, backref='time_worked', verbose_name='Задача', on_delete='CASCADE')
-    term = SmallIntegerField(verbose_name="Продолжительность")
     date = DateField(default=datetime.now, verbose_name='Дата')
+    value = SmallIntegerField(verbose_name="Продолжительность")
 
     def __str__(self):
-        return f'{self.term} ч. ({self.date:%d-%m-%Y})'
+        return f'{self.value} ч. ({self.date:%d-%m-%Y})'
 
     def __add__(self, other):  # обычное сложение с правым элементом
-        return int(self.term + other.term) if isinstance(other, self.__class__) else (
-            int(self.term + other) if isinstance(other, (int, float)) else NotImplemented)
+        return int(self.value + other.value) if isinstance(other, self.__class__) else (
+            int(self.value + other) if isinstance(other, (int, float)) else NotImplemented)
 
     def __radd__(self, other):  # метод для функции sum() и сложение с левым элементом
-        return int(other + self.term) if isinstance(other, (int, float)) else NotImplemented
-
-    def save(self, **kwargs):
-        super().save(**kwargs)
-        self.task.duration = sum(self.task.time_worked)
-        self.task.save()
+        return int(other + self.value) if isinstance(other, (int, float)) else NotImplemented
 
 
 class Turn(BaseModel):
     worker = ForeignKeyField(Person, verbose_name='Работник', backref='turns', on_delete='CASCADE')
     date = DateField(verbose_name='Дата', default=datetime.now)
-    duration = SmallIntegerField(verbose_name="Продолжительность")
+    value = SmallIntegerField(verbose_name="Продолжительность")
 
     def __str__(self):
-        return f'{self.duration} ч.'
+        return f'{self.value} ч.'
 
     def __add__(self, other):  # обычное сложение с правым элементом
-        return int(self.duration + other.duration) if isinstance(other, self.__class__) else (
-            int(self.duration + other) if isinstance(other, (int, float)) else NotImplemented)
+        return int(self.value + other.value) if isinstance(other, self.__class__) else (
+            int(self.value + other) if isinstance(other, (int, float)) else NotImplemented)
 
     def __radd__(self, other):  # метод для функции sum() и сложение с левым элементом
-        return int(other + self.duration) if isinstance(other, (int, float)) else NotImplemented
+        return int(other + self.value) if isinstance(other, (int, float)) else NotImplemented
 
 
 # from database.models import *
@@ -113,42 +106,58 @@ class Turn(BaseModel):
 #     ]
 #     Person.insert_many(data_person).execute()
 #     data_task = [
-#         {'equipment': 'Ру10кВ', 'title': 'Ярино', 'article': 'ENF10_637_03_044_00', 'order': 'ПР-028108', 'deadline': 42, 'master': 1},
-#         {'equipment': 'Ру10кВ ЛЭП АБ', 'title': 'Ярино', 'article': 'ENF10_633_00_000_00-65', 'order': 'ПР-028208', 'deadline': 24, 'master': 2},
-#         {'equipment': 'Ру10кВ', 'title': 'Абакумовка', 'article': 'ENF10_633_00_000_00-65', 'order': 'ПР-028258', 'deadline': 24, 'master': 3},
-#         {'equipment': 'Ру10кВ ЛЭП АБ', 'title': 'Ярино', 'article': 'ENF10_633_00_000_00-65', 'order': 'ПР-028278', 'deadline': 24, 'master': 4},
-#         {'equipment': 'Ру20кВ', 'title': 'ЗИЛ', 'article': 'ENF20_003_00_000_00-03', 'order': 'ПР-028103', 'deadline': 24, 'master': 5},
-#         {'equipment': 'Ру10кВ', 'title': 'Саларьево', 'article': 'ENF10_003_00_000_00-03', 'order': 'ПР-028103', 'deadline': 24, 'master': 1},
-#         {'equipment': 'Ру10кВ', 'title': 'Саянская', 'article': 'ENF10_634_03_022_00', 'order': 'ПР-027111', 'deadline': 42, 'master': 2},
-#         {'equipment': 'Ру10кВ', 'title': 'Саларьево', 'article': 'ENF10_635_00_000_00-03', 'order': 'ПР-028643', 'deadline': 24, 'master': 3},
-#         {'equipment': 'Ру10кВ', 'title': 'Ванино', 'article': 'ENF10_629_00_000_00-18', 'order': 'ПР-028104', 'deadline': 24, 'master': 4},
-#         {'equipment': 'Ру10кВ', 'title': 'Ванино', 'article': 'ENF10_633_00_000_00-67', 'order': 'ПР-028113', 'deadline': 24, 'master': 5},
-#         {'equipment': 'Ру10кВ', 'title': 'Ванино', 'article': '633-67 - 2 шт 629-18 - 2шт', 'order': 'сборка', 'deadline': 54 * 4 // 3, 'master': 6},
-#         {'equipment': 'Ру10кВ', 'title': 'Ванино', 'article': '633-67 - 2 шт 629-18 - 2шт', 'order': 'сборка', 'deadline': 54 * 4 // 3, 'master': 7},
-#         {'equipment': 'Ру10кВ', 'title': 'Ванино', 'article': '633-67 - 2 шт 629-18 - 2шт', 'order': 'сборка', 'deadline': 54 * 4 // 3, 'master': 8},
-#         {'equipment': 'Ру10кВ', 'title': 'Саларьево', 'article': '630-33 - 2шт 632-10 - 1шт', 'order': 'сборка', 'deadline': 54, 'master': 6},
-#         {'equipment': 'Ру10кВ', 'title': 'Саларьево', 'article': '630-33 - 2шт 632-10 - 1шт', 'order': 'сборка', 'deadline': 54, 'master': 7},
-#         {'equipment': 'Ру10кВ', 'title': 'Саларьево', 'article': '630-33 - 2шт 632-10 - 1шт', 'order': 'сборка', 'deadline': 54, 'master': 8},
-#         {'equipment': 'Ру10кВ', 'title': 'Саянская', 'article': '637-40 - 2шт 638-11 - 2шт', 'order': 'сборка', 'deadline': 54 * 4 // 3, 'master': 6},
-#         {'equipment': 'Ру10кВ', 'title': 'Саянская', 'article': '637-40 - 2шт 638-11 - 2шт', 'order': 'сборка', 'deadline': 54 * 4 // 3, 'master': 7},
-#         {'equipment': 'Ру10кВ', 'title': 'Саянская', 'article': '637-40 - 2шт 638-11 - 2шт', 'order': 'сборка', 'deadline': 54 * 4 // 3, 'master': 7},
+#         {'type_obj': 'Ру-10кВ', 'title': 'Ярино', 'article': 'ENF10_637_03_044_00', 'order': 'ПР-028108', 'deadline': 42, 'master': 1},
+#         {'type_obj': 'Ру-10кВ ЛЭП АБ', 'title': 'Ярино', 'article': 'ENF10_633_00_000_00-65', 'order': 'ПР-028208', 'deadline': 24, 'master': 2},
+#         {'type_obj': 'Ру-10кВ', 'title': 'Абакумовка', 'article': 'ENF10_633_00_000_00-65', 'order': 'ПР-028258', 'deadline': 24, 'master': 3},
+#         {'type_obj': 'Ру-10кВ ЛЭП АБ', 'title': 'Ярино', 'article': 'ENF10_633_00_000_00-65', 'order': 'ПР-028278', 'deadline': 24, 'master': 4},
+#         {'type_obj': 'Ру-20кВ', 'title': 'ЗИЛ', 'article': 'ENF20_003_00_000_00-03', 'order': 'ПР-028103', 'deadline': 24, 'master': 5},
+#         {'type_obj': 'Ру-10кВ', 'title': 'Саларьево', 'article': 'ENF10_003_00_000_00-03', 'order': 'ПР-028203', 'deadline': 24, 'master': 1},
+#         {'type_obj': 'Ру-10кВ', 'title': 'Саянская', 'article': 'ENF10_634_03_022_00', 'order': 'ПР-027111', 'deadline': 42, 'master': 2},
+#         {'type_obj': 'Ру-10кВ', 'title': 'Саларьево', 'article': 'ENF10_635_00_000_00-03', 'order': 'ПР-028643', 'deadline': 24, 'master': 3},
+#         {'type_obj': 'Ру-10кВ', 'title': 'Ванино', 'article': 'ENF10_629_00_000_00-18', 'order': 'ПР-028104', 'deadline': 24, 'master': 4},
+#         {'type_obj': 'Ру-10кВ', 'title': 'Ванино', 'article': 'ENF10_633_00_000_00-67', 'order': 'ПР-028113', 'deadline': 24, 'master': 5},
+#         {'type_obj': 'Ру-10кВ', 'title': 'Саянская', 'article': 'ENF10_633_00_000_00-67', 'order': 'ПР-028313', 'deadline': 54, 'master': 6},
+#         {'type_obj': 'Ру-10кВ', 'title': 'Саянская', 'article': 'ENF10_633_00_000_00-67', 'order': 'ПР-028313', 'deadline': 54, 'master': 7},
+#         {'type_obj': 'Ру-10кВ', 'title': 'Саянская', 'article': 'ENF10_633_00_000_00-67', 'order': 'ПР-028313', 'deadline': 54, 'master': 8},
+#         {'type_obj': 'Ру-10кВ', 'title': 'Ванино', 'article': 'ENF10_633_00_000_00-67', 'order': 'ПР-028314', 'deadline': 54, 'master': 6},
+#         {'type_obj': 'Ру-10кВ', 'title': 'Ванино', 'article': 'ENF10_633_00_000_00-67', 'order': 'ПР-028314', 'deadline': 54, 'master': 7},
+#         {'type_obj': 'Ру-10кВ', 'title': 'Ванино', 'article': 'ENF10_633_00_000_00-67', 'order': 'ПР-028314', 'deadline': 54, 'master': 8},
+#         {'type_obj': 'Ру-10кВ', 'title': 'Ванино', 'article': 'ENF10_629_00_000_00-18', 'order': 'ПР-028315', 'deadline': 54, 'master': 6},
+#         {'type_obj': 'Ру-10кВ', 'title': 'Ванино', 'article': 'ENF10_629_00_000_00-18', 'order': 'ПР-028315', 'deadline': 54, 'master': 7},
+#         {'type_obj': 'Ру-10кВ', 'title': 'Ванино', 'article': 'ENF10_629_00_000_00-18', 'order': 'ПР-028315', 'deadline': 54, 'master': 8},
+#         {'type_obj': 'Ру-10кВ', 'title': 'Ванино', 'article': 'ENF10_629_00_000_00-18', 'order': 'ПР-028316', 'deadline': 54, 'master': 6},
+#         {'type_obj': 'Ру-10кВ', 'title': 'Ванино', 'article': 'ENF10_629_00_000_00-18', 'order': 'ПР-028316', 'deadline': 54, 'master': 7},
+#         {'type_obj': 'Ру-10кВ', 'title': 'Ванино', 'article': 'ENF10_629_00_000_00-18', 'order': 'ПР-028316', 'deadline': 54, 'master': 8},
+#         {'type_obj': 'Ру-10кВ', 'title': 'Саларьево', 'article': 'ENF10_633_00_000_00-67', 'order': 'ПР-028317', 'deadline': 54, 'master': 6},
+#         {'type_obj': 'Ру-10кВ', 'title': 'Саларьево', 'article': 'ENF10_633_00_000_00-67', 'order': 'ПР-028317', 'deadline': 54, 'master': 7},
+#         {'type_obj': 'Ру-10кВ', 'title': 'Саларьево', 'article': 'ENF10_633_00_000_00-67', 'order': 'ПР-028317', 'deadline': 54, 'master': 8},
 #     ]
 #     WorkTask.insert_many(data_task).execute()
-#     work_lapse_data = []
+#
+#     import datetime
 #     import random
+#     target_date = datetime.date(2024, 7, 11)
+#     work_lapse_data = []
 #     for _ in range(50):
-#         task = random.randint(1, 19)
-#         term = random.randint(1, 12)
-#         month = random.randint(1, 12)
-#         day = random.randint(1, 15)
-#         laps = {'worker': WorkTask[task].master, 'task': WorkTask[task], 'term': term, 'date': f'2024-{month}-{day}'}
-#         WorkLapse.create(**laps)
-        # work_lapse_data.append(laps)
-    # WorkLapse.insert_many(work_lapse_data).execute()
+#         for worker in Person.select():
+#             print(f'{worker}')
+#             if random.random() > 0.05:
+#                 print("сработало")
+#                 task = random.choice(worker.tasks)
+#                 duration = random.choice([8, 12])
+#                 print(f'{task=} {duration=}')
+#                 if sum(task.time_worked) < 1.1 * task.deadline:
+#                     laps = {'worker': worker, 'task': task, 'value': duration, 'date': target_date}
+#                     work_lapse_data.append(laps)
+#         target_date += datetime.timedelta(days=1)
+#     for i in range(0, len(work_lapse_data), 50):
+#         WorkLapse.insert_many(work_lapse_data[i:i + 50]).execute()
+
+
 
 # tasks = WorkTask.select(WorkTask.id, WorkTask.order, WorkTask.duration, fn.SUM(WorkLapse.term).alias('total')).join(WorkLapse, JOIN.LEFT_OUTER).group_by(WorkTask.id).dicts()
 # tasks = WorkTask.select(WorkTask, Status).join(Status)
 # laps = WorkLapse.select()
 # result = prefetch(tasks, laps)
-
+#
 # ts = WorkTask.select(WorkTask.order, WorkTask.duration, WorkTask.time_worked, fn.SUM(WorkTask.time_worked).alias('summ'))
--- database/queries.py
+++ database/queries.py
@@ -3,14 +3,11 @@ from .models import Person, FuncPosition, WorkTask, Status, WorkLapse
 
 
 def get_all_workers():
-    person = Person.select(Person, FuncPosition.title).join(FuncPosition)
-    tasks = WorkTask.select(WorkTask, Status).join(Status).where(Status.state == 'В работе')
-    # return peewee.prefetch(person, tasks)
     return (
         Person.select(
             Person.id, Person.surname, Person.name, Person.second_name,
-            FuncPosition.title,
-            WorkTask.order, WorkTask.deadline, peewee.fn.SUM(WorkLapse.term).alias('total'),
+            FuncPosition.title.alias('post'),
+            WorkTask.order, WorkTask.deadline, peewee.fn.SUM(WorkLapse.value).alias('total'),
             Status.state,
         )
         .join_from(Person, FuncPosition)
@@ -25,27 +22,18 @@ def get_all_workers():
 def get_all_tasks():
     return (
         WorkTask.select(
-            WorkTask.id, WorkTask.equipment, WorkTask.title, WorkTask.article, WorkTask.order,
-            WorkTask.deadline, peewee.fn.SUM(WorkLapse.term).alias('total'),
+            WorkTask.id, WorkTask.type_obj, WorkTask.title, WorkTask.article, WorkTask.order,
+            WorkTask.deadline, peewee.fn.SUM(WorkLapse.value).alias('total'),
             Status.state,
-            Person.surname, Person.name, Person.second_name
+            Person.surname, Person.name, Person.second_name,
+            FuncPosition.title.alias('post')
         )
         .join_from(WorkTask, WorkLapse, peewee.JOIN.LEFT_OUTER)
         .join_from(WorkTask, Status)
         .join_from(WorkTask, Person)
+        .join_from(Person, FuncPosition)
         .where(Status.state == 'В работе')
         .group_by(WorkTask.id).dicts()
-
-        # WorkTask.select(
-        #     WorkTask.id, WorkTask.equipment, WorkTask.title, WorkTask.article, WorkTask.order, WorkTask.deadline,
-        #     WorkTask.duration,
-        #     Status.state,
-        #     Person.surname, Person.name, Person.second_name
-        # )
-        # .join(Status)
-        # .where(Status.state == 'В работе')
-        # .switch(WorkTask)
-        # .join(Person)
     )
 
 
