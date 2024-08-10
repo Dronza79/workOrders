b1539cfa5a095c24ff3b051941e96d65625f782c
Dronza79 <113519920+Dronza79@users.noreply.github.com>
2024-08-10 15:54:06 +0300

    Add files via upload

--- main.py
+++ main.py
@@ -11,8 +11,7 @@ def main():
     # print(f'{path.get_path=}')
     if not Path(path.get_path).exists():
         apply_migrations()
-    main_window = StartMainWindow()
-    main_window.run()
+    StartMainWindow()
 
 
 if __name__ == '__main__':
