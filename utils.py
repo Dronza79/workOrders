3b53f422bc3afae33033ea21a41cd20e8a7a5005
Dronza79 <113519920+Dronza79@users.noreply.github.com>
2024-10-09 16:59:30 +0300

    конец дня

---
+++ utils.py
@@ -0,0 +1,174 @@
+import re
+from datetime import datetime as dt
+
+from database.models import Worker, Vacancy, Order, Task, Status, Period, TypeTask
+
+
+def validation_data(raw_data, idx=None):
+    errors = []
+    valid_data = {}
+    entity = raw_data.get('type')
+    if entity == 'worker':
+        dep = {
+            'surname': 'Фамилия',
+            'name': 'Имя',
+            'table_num': 'Табельный номер',
+            'second_name': 'Отчество',
+            'function': 'Должность'
+        }
+        worker = None
+        if idx:
+            worker = Worker.get(Worker.id == idx)
+        for key in ['surname', 'name', 'second_name', 'table_num', 'function']:
+            if key != 'function':
+                if key != 'second_name' and len(raw_data[key].strip()) < 3:
+                    errors.append(f'Ошибка:\nПоле {dep[key]} не заполнено! (мин. 3 буквы)\n')
+                elif key in ['surname', 'name'] or (key == 'second_name' and raw_data['second_name'].strip()):
+                    if not re.findall(r'\b[А-Яа-я]+\b', raw_data[key].strip()):
+                        errors.append(f'Ошибка:\nПоле {dep[key]} допускаются только русские буквы!\n')
+                    else:
+                        if (idx and getattr(worker, key).lower() != raw_data[key].strip().lower()) or not idx:
+                            valid_data[key] = raw_data[key].strip().capitalize()
+                elif key == 'table_num':
+                    if not idx or getattr(worker, key) != raw_data[key].strip():
+                        if Worker.get_or_none(Worker.table_num == raw_data[key].strip().capitalize()):
+                            errors.append(f'Ошибка:\nРаботник с таким Табельным номером уже существует!\n')
+                        else:
+                            if (idx and getattr(worker, key).lower() != raw_data[key].strip().lower()) or not idx:
+                                valid_data[key] = raw_data[key].strip().capitalize()
+            else:
+                if not isinstance(raw_data[key], Vacancy):
+                    errors.append(f'Ошибка:\nПоле {dep[key]} не выбрано!\n')
+                else:
+                    if (idx and getattr(worker, key) != raw_data[key]) or not idx:
+                        valid_data[key] = raw_data[key]
+
+    elif entity == 'order':
+        dep = {
+            'no': 'Номер производственного заказа',
+            'type_obj': 'Тип объекта',
+            'title': 'Наименование объекта',
+            'article': 'Конструктив',
+        }
+        order = None
+        if idx:
+            order = Order.get(Order.id == idx)
+        for key in ['no', 'type_obj', 'title', 'article']:
+            if len(raw_data[key]) < 3:
+                errors.append(f'Ошибка:\nПоле {dep[key]} не заполнено!\n')
+            elif not re.findall(r'\b[А-Яа-я]+\b', raw_data[key]) and key in ['type_obj', 'title']:
+                errors.append(f'Ошибка:\nПоле {dep[key]} допускаются только русские буквы!\n')
+            elif key == 'article':
+                if not re.findall(r'\b[A-Za-z]{3}\d{2}[_-]\d{3}[_-]\d{2}[_-]\d{3}[_-]\d{2}', raw_data[key]):
+                    errors.append(f'Ошибка:\nПоле {dep[key]} заполнено не по формату!\n')
+                else:
+                    if (idx and getattr(order, key) != raw_data[key]) or not idx:
+                        valid_data[key] = raw_data[key].upper()
+            elif key == 'no':
+                if not re.findall(r'\d+', raw_data[key]):
+                    errors.append(f'Ошибка:\nПоле {dep[key]} заполнено не по формату!\n')
+                elif not idx or order.to_order != raw_data['no']:
+                    # print('Проверка номера=', Order.get_or_none(Order.no == int(re.findall(r'\d+', raw_data['no']).pop())))
+                    if Order.get_or_none(Order.no == int(re.findall(r'\d+', raw_data['no']).pop())):
+                        errors.append(f'Ошибка:\nЗаказ с таким номером уже существует!\n')
+                    else:
+                        if (idx and getattr(order, key) != raw_data[key]) or not idx:
+                            # print('получение номера=', int(re.findall(r'\d+', raw_data[key]).pop()))
+                            valid_data[key] = int(re.findall(r'\d+', raw_data[key]).pop())
+            else:
+                if (idx and getattr(order, key) != raw_data[key]) or not idx:
+                    valid_data[key] = raw_data[key]
+
+    elif entity == 'task':
+        dep = {
+            'is_type': 'Тип задачи',
+            'order': 'Производственный заказ',
+            'worker': 'Исполнитель',
+            'status': 'Статус',
+            'deadline': 'Норматив выполнения',
+            'comment': 'Комментарии'
+        }
+        task = None
+        if idx:
+            task = Task.get_by_id(idx)
+        for key, value in {'order': Order, 'status': Status, 'worker': Worker, 'is_type': TypeTask}.items():
+            # print(f'{key=} {value=} {raw_data[key]=}')
+            if idx:
+                if key == 'status' and task.status != raw_data[key]:
+                    valid_data[key] = raw_data[key]
+            else:
+                if not isinstance(raw_data[key], value) and key != 'order':
+                    errors.append(f'Ошибка:\nПоле {dep[key]} не выбрано!\n')
+                elif key == 'order':
+                    if isinstance(raw_data[key], value):
+                        valid_data[key] = raw_data[key]
+                else:
+                    valid_data[key] = raw_data[key]
+
+        print(f'{valid_data=}')
+        if not raw_data['deadline'].isdigit():
+            errors.append(f'Ошибка:\nПоле {dep["deadline"]} допускаются только цифры!\n')
+        else:
+            if (idx and getattr(task, "deadline") != int(raw_data["deadline"])) or not idx:
+                valid_data["deadline"] = int(raw_data["deadline"])
+        if (idx and getattr(task, "comment") != raw_data["comment"]) or not idx:
+            valid_data["comment"] = raw_data["comment"]
+
+    return errors, valid_data
+
+
+def validation_period_data(raw_data, idx=None):
+    print(f'validation_period_data {raw_data=}')
+    valid_data = {}
+    errors = []
+    date = raw_data.get('date')
+    period = None
+    if idx:
+        period = Period[int(idx)]
+        valid_data['period_id'] = raw_data['period_id']
+    else:
+        task = raw_data.get('task')
+        valid_data['worker'] = task.worker
+        valid_data['task'] = task
+        valid_data['order'] = task.order
+    if (idx and getattr(period, 'value') != raw_data.get('value')) or not idx:
+        valid_data['value'] = raw_data.get('value')
+    if re.findall(r'\b\d{2}\.\d{2}\.\d{4}\b', date):
+        if (idx and getattr(period, 'date') != dt.strptime(date, '%d.%m.%Y').date()) or not idx:
+            valid_data['date'] = dt.strptime(date, '%d.%m.%Y').date()
+    else:
+        errors.append('Ошибка.\nДата указана не верно!\nВоспользуйтесь кнопкой Календарь!')
+
+    return errors, valid_data
+
+
+def validation_data_for_exel(raw_data):
+    print(f'{raw_data=}')
+    valid_data = {}
+    errors = []
+    dep = {
+        '-from-': '"от:"',
+        '-to-': '"по:"',
+    }
+    if not isinstance(raw_data['-worker-'], Worker):
+        errors.append('Ошибка.\nВы выбрали что то не то)!')
+    else:
+        valid_data['worker'] = raw_data['-worker-']
+    for key in dep:
+        print(f'{key=}')
+        print(f'{raw_data[key]=}')
+        if not raw_data[key]:
+            errors.append(f'Ошибка:\nПоле {dep[key]} не заполнено!\n')
+        elif not re.findall(r'\b\d{2}\.\d{2}\.\d{4}\b', raw_data[key]):
+            errors.append(f'Ошибка:\nПоле {dep[key]} заполнено не по формату!\nВоспользуйтесь кнопкой "Календарь"')
+            break
+        elif key == '-to-':
+            date_from = dt.strptime(raw_data['-from-'], '%d.%m.%Y').date()
+            date_to = dt.strptime(raw_data['-to-'], '%d.%m.%Y').date()
+            if date_from >= date_to:
+                errors.append(f'Ошибка:\nДата "от:" не может быть позже\nили равна дате "по:"')
+            else:
+                valid_data['from'] = date_from
+                valid_data['to'] = date_to
+
+    return errors, valid_data
